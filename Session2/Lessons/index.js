2be56db6e586fba4da8b4fab818a6d9353a929e6
Macrosis2020 <professionalmothlover2511@gmail.com>
2020-12-17 21:52:41 +0700

    Add Hw1-13

---
+++ Session2/Lessons/index.js
@@ -0,0 +1,25 @@
+/**while - Infinite Loop
+ while (true) {
+    <will run infinitely>
+ } 
+ 
+ while - Useless Loop
+ while (false) {
+     <won't run at all>
+ }*/
+// let x = 1;
+// while (x <= 5) {
+// console.log(`Hello World! x${x}`);
+// x++;
+// }
+// for (let i = 1; i <= 10; i++) {
+// console.log(`Hello World! x${i}`);
+// }
+let age = prompt('Bạn bao nhiêu tuổi?');
+if (age < 18) {
+    alert('Cút');
+} else if (age < 60) {
+    alert('Welcome to Family Guy Funny Moments #3534323');
+} else {
+    alert('Bugger off, grandpa');
+}
